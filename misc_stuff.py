4ff69bbdd669569ecbc1f68308c469ee46bc52cb
Ian Daniher <it.daniher@gmail.com>
2011-02-01 23:38:59 -0500

    added a few more example uses

--- misc_stuff.py
+++ misc_stuff.py
@@ -2,9 +2,21 @@ import smu, numpy, pickle
 
 smu = smu.smu()
 
+data = {}
+
+for v in numpy.arange(1, 5, .05):
+	smu.set_voltage(1, v)
+	data[v]=smu.get_voltage(2)
+
+for v in numpy.arange(1, 5, .05):
+	smu.set_current(2, 0)
+    smu.set_voltage(1, v)  
+    data[v]=smu.get_voltage(2)
+
 for a in numpy.arange(.0001, .01, .00005):
-	smu.set_current(1, a)
-	data[a]=smu.get_voltage(1)
+    smu.set_voltage(2, 0)
+    smu.set_current(1, a)
+    data[a]=smu.get_current(2)
 
 
 pickle.dump( data, open( "exp1.p", "wb" ) )
