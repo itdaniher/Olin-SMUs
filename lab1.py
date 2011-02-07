2f66ede77099c749543b4e9052d81e52dfe8c860
Ian Daniher <it.daniher@gmail.com>
2011-02-07 13:04:53 -0500

    moved misc_stuff.py to lab1.py

---
+++ lab1.py
@@ -0,0 +1,85 @@
+import smu, pylab, pickle
+
+smu = smu.smu()
+
+data = {}
+
+for v in pylab.arange(1, 5, .05):
+	smu.set_voltage(1, v)
+	data[v]=smu.get_voltage(2)
+
+for v in pylab.arange(1, 5, .05):
+	smu.set_current(2, 0)
+    smu.set_voltage(1, v)  
+    data[v]=smu.get_voltage(2)
+
+for a in pylab.arange(.0001, .01, .00005):
+    smu.set_voltage(2, 0)
+    smu.set_current(1, a)
+    data[a]=smu.get_current(2)
+
+
+pickle.dump( data, open( "exp1.p", "wb" ) )
+##Begin Data Analysis
+
+def fig1():
+	data1 = pickle.load(open("exp1.p"))
+	clean = dict((k,v) for k,v in data1.items() if v < 7.0)
+	(m, b) = pylab.polyfit(clean.keys(), clean.values(), 1)
+	fit = dict((k,k*m+b) for k,v in clean.items())
+	pylab.plot(clean.keys(), clean.values(), 'k.', label='experimental data')
+	pylab.plot(fit.keys(), fit.values(), 'r', label='fit line, slope of %.3f V/I($\Omega$)' % m)
+	pylab.legend(loc = 'upper left')
+	pylab.xlabel('current (amps)')
+	pylab.ylabel('voltage (volts)')
+	pylab.title("Experiment 1 - V-I curve for a 10k Resistor")
+
+
+def fig2():
+	data2 = pickle.load(open("exp2.p"))
+	clean = dict((k,v) for k,v in data2.items() if .4<v/k<.6)
+	(m, b) = pylab.polyfit(clean.keys(), clean.values(), 1)
+	fit = dict((k,k*m+b) for k,v in clean.items())
+	pylab.plot(clean.keys(), clean.values(), 'k.', label='experimental data')
+	pylab.plot(fit.keys(), fit.values(), 'r', label='fit line, slope of %.3f' % m)
+	pylab.legend(loc = 'upper left')
+	pylab.xlabel('$V_{out}$ (volts)')
+	pylab.ylabel('$V_{in}$ (volts)')
+	pylab.title("Experiment 2 - V-V curve for a Voltage Divider")
+
+def fig3():
+	data3 = pickle.load(open("exp3.p"))
+	clean = dict((k,abs(v)) for k,v in data3.items() if 0.00035<k<0.0009)
+	(m, b) = pylab.polyfit(clean.keys(), clean.values(), 1)
+	fit = dict((k,k*m+b) for k,v in clean.items())
+	pylab.plot(clean.keys(), clean.values(), 'k.', label='experimental data')
+	pylab.plot(fit.keys(), fit.values(), 'r', label='fit line, slope of %.3f' % m)
+	pylab.legend(loc = 'upper left')
+	pylab.xlabel('$I_{out}$ (amps)')
+	pylab.ylabel('$I_{in}$ (amps)')
+	pylab.title("Experiment 3 I-I curve for a Current Divider")
+
+def fig3Alt():
+	clean = pickle.load(open("exp3.p"))
+	pylab.plot(clean.keys(), clean.values(), 'k.', label='raw experimental data')
+	pylab.legend(loc = 'upper left')
+	pylab.xlabel('$I_{out}$ (amps)')
+	pylab.ylabel('$I_{in}$ (amps)')
+	pylab.title("Experiment 3 - Raw Data")
+
+def fig4():
+	n1 = pickle.load(open("exp4.1.p"))
+	n2 = pickle.load(open("exp4.2.p"))
+	n3 = pickle.load(open("exp4.3.p"))
+	n4 = pickle.load(open("exp4.4.p"))
+	for num in n1.keys():
+		v = 'v%s' % num
+		vars()[v] = [n1[num], n2[num], n3[num], n4[num]]
+		if num > 3:
+			pylab.semilogy(vars()[v], 'o', label="%s volts" % num)
+	pylab.legend(loc = 'upper right')
+	pylab.xlabel('"2R" Number')
+	pylab.ylabel('Current Measured (amps)')
+	pylab.title("Experiment 4 : Current through R-2R Ladder Branches")
+	pylab.xticks([-1, 0, 1, 2, 3, 4])
+	pylab.yticks([1e-5, 1e-4, 1e-3])
