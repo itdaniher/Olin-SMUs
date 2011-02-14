c1824b631a2831efd8f0b9349b9423696ca7db6c
Ian Daniher <it.daniher@gmail.com>
2011-02-14 04:09:29 -0500

    got semilog data fitting working. save early, save often.

--- lab2.py
+++ lab2.py
@@ -1,49 +1,53 @@
-import smu, pylab
+import smu
+import pickle
+from import *
 
 smu = smu.smu()
 
 data = {}
 
-for amp in pylab.logspace(-9, -2, 60):
+for amp in logspace(-9, -2, 60):
     smu.set_current(1, amp)
     data[amp] = smu.get_voltage(1)
 
 pickle.dump( data, open( "exp1.1.p", "wb" ) )
 
-pylab.semilogy(data.values(), data.keys(), 'k.')
+semilogy(data.values(), data.keys(), 'k.')
 
-for volt in pylab.arange(.2, .7, .005):
+for volt in arange(.2, .7, .005):
 	smu.set_voltage(1, volt)
 	data[volt] = smu.get_current(1)
 
 pickle.dump( data, open( "exp1.2.p", "wb" ) )
 
-pylab.semilogx(data.keys(), data.values(), 'k.')
+semilogx(data.keys(), data.values(), 'k.')
 
 clean = dict((k,v/k) for k,v in data.items())
 
-pylab.loglog(clean.keys(), clean.values(), 'k.')
+loglog(clean.keys(), clean.values(), 'k.')
 
 #begin data analysis / plotting
 
 exp1data1 = pickle.load(open("exp1.1.p"))
-pylab.semilogx(exp1data1.keys(), exp1data1.values(), 'k.')
-pylab.title('current-source V-I characterization of a pseudodiode')
-pylab.ylabel('Voltage(V)')
-pylab.xlabel('Log of Current(I)')
+semilogx(exp1data1.keys(), exp1data1.values(), 'k.', label='experimental data')
+title('current-source V-I characterization of a pseudodiode')
+ylabel('Voltage(V)')
+xlabel('Log of Current(I)')
 
-exp1data1_cleanlog = dict((pylab.log(k),v) for k,v in exp1data1.items() if v > 0.1)  
-
+exp1data1_cleanlog = dict((ln(k),v) for k,v in exp1data1.items() if v > 0.1)  
+(m, b) = polyfit(exp1data1_cleanlog.keys(), exp1data1_cleanlog.values(), 1)
+fit = dict((k,k*m+b) for k,v in exp1data1_cleanlog.items())
+plot(fit.keys(), fit.values(), 'r', label='fit line, slope of %.3f' % m)
 
 
 exp1data2 = pickle.load(open("exp1.2.p"))
-pylab.semilogy(exp1data2.keys(), exp1data2.values(), 'k.')
-pylab.title('voltage-source V-I characterization of a pseudodiode')
-pylab.ylabel('Log of Current (I)')
-pylab.xlabel('Voltage(V)')
+semilogy(exp1data2.keys(), exp1data2.values(), 'k.')
+title('voltage-source V-I characterization of a pseudodiode')
+ylabel('Log of Current (I)')
+xlabel('Voltage(V)')
 
-#pylab.log on current, polyfit to get U_T and I_s
-#pylab.diff(V)/pylab.diff(I)
+#log on current, polyfit to get U_T and I_s
+#diff(V)/pylab.diff(I)
 
 hund = pickle.load(open("exp2.1.p"))
 #hund
@@ -54,6 +58,6 @@ tenk = pickle.load(open("exp2.3.p"))
 hunk = pickle.load(open("exp2.4.p")) 
 #hunk
 
-pylab.plot(hund[0].keys(), hund[0].values(), 'ro', label='100 $\Omega$')
-pylab.plot(onek[0].keys(), onek[0].values(), 'b^', label='1k $\Omega$')
-pylab.plot(tenk[0].keys(), tenk[0].values(), 'gs', label='10k $\Omega$')
+plot(hund[0].keys(), hund[0].values(), 'ro', label='100 $\Omega$')
+plot(onek[0].keys(), onek[0].values(), 'b^', label='1k $\Omega$')
+plot(tenk[0].keys(), tenk[0].values(), 'gs', label='10k $\Omega$')
