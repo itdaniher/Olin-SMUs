a50b6a49fefb0b73111d89ba69b8fcbec5d86a46
Ian Daniher <it.daniher@gmail.com>
2011-02-15 20:04:31 -0500

    ah, yes. that's what functions are for...

--- lab3.py
+++ lab3.py
@@ -7,17 +7,20 @@ smu = smu.smu()
 #ch1 → base
 #ch2 → emitter to ground
 
-I_b = []
-I_e = []
-I_c = []
-V_b = arange(0, 1, .001)
+def tranSweep(V_i, V_o, V_step, fileName):
+	I_b = []
+	I_e = []
+	I_c = []
+	V_b = arange(V_i, V_o, V_step)
+	for volt in V_b:
+		smu.set_voltage(1, volt)
+		I_b.append(smu.get_current(1))
+		I_e.append(smu.get_current(2))
+		#KCL Muthafucka, KCL Goddamn! -cdt
+		I_c.append(I_e[-1]-I_b[-1])
+	pickle.dump( (I_b, I_e, I_c, V_b) , open( fileName, "wb" ) )
 
-for volt in V_b:
-	smu.set_voltage(1, volt)
-	I_b.append(smu.get_current(1))
-	I_e.append(smu.get_current(2))
-	#KCL Muthafucka, KCL Goddamn! -cdt
-	I_c.append(I_e[-1]-I_b[-1])
+tranSweep(0, 1, .001, "exp1.p")
 
 
 
