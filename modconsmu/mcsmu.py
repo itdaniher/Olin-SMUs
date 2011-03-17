4c7bd34f82491d1fdb204a329ddb8532b65be5d4
Ian Daniher <it.daniher@gmail.com>
2011-03-17 16:25:03 -0400

    code refactor not yet completed.

--- modconsmu/mcsmu.py
+++ modconsmu/mcsmu.py
@@ -2,7 +2,7 @@
 
 import sys
 import usb.core
-import array
+import numpy 
 """
 if (libusb_control_transfer(udev[i], 0x00, 0x09, 1, 0, buffer, 0, 0) < 0) {
 if (libusb_control_transfer(udev[i], 0x40, SET_DIGOUT, digout, 0, buffer, 0, 0) < 0) {
@@ -30,28 +30,51 @@ class smu:
 	minV = -10.45
 	scaleFactorV = (minV-maxV)/(2**12)
 
+	v = 0
+	i = 0
+	master = "i"
+
+
+	def sign(self, x):
+		if x > 32767:
+			return 65536-x
+		else:
+			return x
+
 	def __init__(self):
 		"""Find a USB device with the VID and PID of the ModCon SMU."""
 		self.dev = usb.core.find(idVendor=0x6666, idProduct=0x0005)
 		if self.dev is None:
 			sys.exit("Can't find ModCon SMU")
-		self.set(self.zero)
+		VADC_VALS = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['GET_VADC_VALS'], wValue = 0, wIndex = 0, data_or_wLength = 4)
+		IADC_VALS = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['GET_IADC_VALS'], wValue = 0, wIndex = 0, data_or_wLength = 4)
+		RES_VAL = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['GET_RES_VAL'], wValue = 0, wIndex = 0, data_or_wLength = 2)
+		DAC_VALS = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['GET_DAC_VALS'], wValue = 0, wIndex = 0, data_or_wLength = 4)
+		self.DAC = ( DAC_VALS[0] | ( DAC_VALS[1] << 8 ) ) / 16.0
+		VALUE = self.sign( DAC_VALS[2] | DAC_VALS[3] << 8 )
+		self.DACGAIN = -200*VALUE/16384.0
+		self.VADC = ( VADC_VALS[0] | ( VADC_VALS[1] << 8 ) ) / 16.0
+		VALUE = self.sign( VADC_VALS[2] | ( VADC_VALS[3] << 8 ) )
+		self.VADCGAIN = 50*VALUE/16384.0
+		self.IADC = ( IADC_VALS[0] | ( IADC_VALS[1] << 8 ) ) / 16.0
+		VALUE = self.sign( IADC_VALS[2] | ( IADC_VALS[3] << 8 ) )
+		self.IADCGAIN = 50*VALUE/16384.0
+		VALUE = self.sign( RES_VAL[0] | ( RES_VAL[1] << 8 ) )
+		self.RES = 51*VALUE/16384.0
+		
 
-	def set(self, value = zero, type = 'i'):
-		if type == 'v':
+	def update(self):
+		if self.master == "v":
+			value = int(self.zero + self.v/self.scaleFactorV)
 			direction = 0
-			value = int(self.zero + value/self.scaleFactorV) 
-		elif type == 'i':
+		elif self.master == "i":
+			value = int(self.zero + self.i * 10000)
 			direction = 1
-			value = int(self.zero + value * 10000)
 		else:
 			print("bad type")
-		buffer = array.array('b', [0, 0, 0, 0])
-		self.dev.ctrl_transfer(bmRequestType = 0x40, bRequest = self.vReqs['SET_DIGOUT'], wValue = direction, wIndex = 0, data_or_wLength = buffer)
-		return self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['UPDATE'], wValue = value, wIndex = 0, data_or_wLength = 12)
-
-	def get(self):
-		VADC_VALS = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['GET_VADC_VALS'], wValue = 0, wIndex = 0, data_or_wLength = 8, timeout = None)
-		IADC_VALS = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['GET_IADC_VALS'], wValue = 0, wIndex = 0, data_or_wLength = 8, timeout = None) 
-		RES_VAL = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['GET_RES_VAL'], wValue = 0, wIndex = 0, data_or_wLength = 8, timeout = None) 
-		return (IADC_VALS, VADC_VALS, RES_VAL)
+		data = numpy.zeros(12)
+		self.dev.ctrl_transfer(bmRequestType = 0x40, bRequest = self.vReqs['SET_DIGOUT'], wValue = direction, wIndex = 0, data_or_wLength = data)
+		data = self.dev.ctrl_transfer(bmRequestType = 0xC0, bRequest = self.vReqs['UPDATE'], wValue = value, wIndex = 0, data_or_wLength = 12)
+		retVolt = ((data[0]|data[1]<<8)-self.VADC)/self.VADCGAIN
+		retAmp = ((data[4]|data[5]<<8)-self.IADC)/(self.IADCGAIN*self.RES)
+		return (retVolt, retAmp)
