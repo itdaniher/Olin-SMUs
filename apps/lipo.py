81ae49a28517ee36e9df6d76bdbade44122d4db7
Ian Daniher <it.daniher@gmail.com>
2011-03-27 03:12:04 -0400

    modifications and further disclaimers.

--- apps/lipo.py
+++ apps/lipo.py
@@ -1,10 +1,22 @@
+#Code by Ian Daniher - 2011-03-26
+
+#Use this code at your own risk!
+
+#This is a proof of concept implementation of the standard LiPo CC/CV charging algorithm.
+
+#Tested to work with a 1000maAh / 3.7v cellphone battery. YMMV!
+
+
 import modconsmu
 import time
 import threading
-import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("km-dockstar", 8098))
+dataStream = 1
+
+if dataStream:
+	import socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect(("km-dockstar", 8098))
 
 smu = modconsmu.smu()
 
@@ -17,28 +29,39 @@ if raw_input("do you absolve the author of this code of all responsibility relat
 def log():
 	#time, (voltage, current)
 	data = ( time.time(), smu.update() )
-	upData = "%f %f %f" % (data[0], data[1][0], abs(data[1][1]))
-	sock.send(upData)
 	chargeData.append(data)
+	if dataStream:
+		upData = "%f %f %f" % (data[0], data[1][0], abs(data[1][1]))
+		sock.send(upData)
 
 def chargeProfile():
+	#log initial open-cell voltage
 	log()
+	#constant current mode starts
 	smu.set(amps = .2)
+	#check cell voltage ever tick until it reaches 4.2v or higher
 	while smu.update()[0] < 4.2:
 		log()
 		time.sleep(tick)
+	#constant voltage mode starts
 	smu.set(volts = 4.2)
+	#check charge current every tick until its 50ma or less
 	while abs(smu.update()[1]) > .05:
 		log()
 		time.sleep(tick)
+	#disable charging
 	smu.set(amps = 0)
+	#make sure that a battery was attached
 	if ( chargeData[-1][0] - chargeData[0][0] ) > 10:
 		log()
 		print("charging done!")
 	else:
+		log()
 		print("no battery attached?")
+		print(chargeData)
 
 def charge():
+	#use threading to allow for non-blocking while-loop checks
 	chargeThread = threading.Thread(target=chargeProfile)
 	chargeThread.daemon = True
 	chargeThread.start()
