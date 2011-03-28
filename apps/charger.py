0f76f1ec689e592f8ac999503d3fa3bdd7cddea7
Ian Daniher <it.daniher@gmail.com>
2011-03-28 17:12:30 -0400

    heavily modified charger.py

--- apps/charger.py
+++ apps/charger.py
@@ -6,45 +6,45 @@
 
 #Tested to work with a 1000maAh / 3.7v cellphone battery. YMMV!
 
-
 import modconsmu
 import time
 import threading
+
+if raw_input("do you absolve the author of this code of all responsibility related to its use? (yes/no)") != "yes":
+	quit()
 
 dataStream = 1
 
 if dataStream:
 	import socket
+	host = "localhost"
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect(("km-dockstar", 8098))
+	sock.connect((host, 8098))
 
 smu = modconsmu.smu()
 
 chargeData = []
 tick = 1
 
-if raw_input("do you absolve the author of this code of all responsibility related to its use? (yes/no)") != "yes":
-	quit()
-
 def log():
 	#time, (voltage, current)
 	data = ( time.time(), smu.update() )
 	chargeData.append(data)
 	if dataStream:
-		upData = "%f %f %f" % (data[0], data[1][0], abs(data[1][1]))
+		upData = "%.4f %.4f %.4f" % (data[0], data[1][0], abs(data[1][1]))
 		sock.send(upData)
 
-def chargeProfile():
+def CCCVProfile(constantCurrent = .2, constantVoltage = 4.2):
 	#log initial open-cell voltage
 	log()
 	#constant current mode starts
-	smu.set(amps = .2)
+	smu.set(amps = constantCurrent)
 	#check cell voltage ever tick until it reaches 4.2v or higher
-	while smu.update()[0] < 4.2:
+	while smu.update()[0] < constantVoltage:
 		log()
 		time.sleep(tick)
 	#constant voltage mode starts
-	smu.set(volts = 4.2)
+	smu.set(volts = constantVoltage)
 	#check charge current every tick until its 50ma or less
 	while abs(smu.update()[1]) > .05:
 		log()
@@ -62,7 +62,7 @@ def chargeProfile():
 
 def charge():
 	#use threading to allow for non-blocking while-loop checks
-	chargeThread = threading.Thread(target=chargeProfile)
+	chargeThread = threading.Thread(target=CCCVProfile)
 	chargeThread.daemon = True
 	chargeThread.start()
 
