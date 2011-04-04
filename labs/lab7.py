5f5e6eb610bc9ff18d6fcd1b82d4485af4e87d5b
Ian Daniher <it.daniher@gmail.com>
2011-04-04 00:31:20 -0400

    woo pFETs! lab7.py for pFETs committed.

--- labs/lab7.py
+++ labs/lab7.py
@@ -2,7 +2,6 @@ from numpy import linspace
 import circuitsmu
 from scipy import io as sio
 
-
 #s01 - V1
 #s02 - V2
 #s11 - Vout1
@@ -14,16 +13,16 @@ smu1 = circuitsmu.smu(1)
 def getState():
 	return (smu0.get_voltage(1), smu0.get_current(1), smu0.get_voltage(2), smu0.get_current(2), smu1.get_voltage(1), smu1.get_current(1), smu1.get_voltage(2), smu1.get_current(2))
 
-smu1.set_voltage(1, 5)
+smu1.set_voltage(1, 0)
 smu1.set_current(2, 0)
-#Vb = 0.673
+#Vb = 4.30
 
 VbBelowThresh = [[], [], []]
 
 numV2s = 3
 numV1s = 300
 
-V2s = linspace(2.5, 3.5, numV2s).tolist()
+V2s = linspace(1.5, 2.5, numV2s).tolist()
 V1s = linspace(2.0, 4, numV1s).tolist()
 
 for V2 in V2s:
@@ -32,7 +31,7 @@ for V2 in V2s:
 		smu0.set_voltage(2, V1)
 		VbBelowThresh[V2s.index(V2)].append(getState())
 
-sio.savemat("nFET-VbBelowThresh-Vout1.mat", mdict={'VbBelowThresh': VbBelowThresh})
+sio.savemat("pFET-VbBelowThresh-Vout1.mat", mdict={'VbBelowThresh': VbBelowThresh})
 
 #s11 - Vout2
 
@@ -44,9 +43,9 @@ for V2 in V2s:
 		smu0.set_voltage(2, V1)
 		VbBelowThresh[V2s.index(V2)].append(getState())
 
-sio.savemat("nFET-VbBelowThresh-Vout2.mat", mdict={'VbBelowThresh': VbBelowThresh})
+sio.savemat("pFET-VbBelowThresh-Vout2.mat", mdict={'VbBelowThresh': VbBelowThresh})
 
-#Vb = 0.803
+#Vb = 4.125
 #s11 - Vout1
 VbAboveThresh = [[], [], []]
 
@@ -56,7 +55,7 @@ for V2 in V2s:
         smu0.set_voltage(2, V1)
         VbAboveThresh[V2s.index(V2)].append(getState())
 
-sio.savemat("nFET-VbAboveThresh-Vout1.mat", mdict={'VbAboveThresh': VbAboveThresh})
+sio.savemat("pFET-VbAboveThresh-Vout1.mat", mdict={'VbAboveThresh': VbAboveThresh})
 
 #s11 - Vout2
 VbAboveThresh = [[], [], []]
@@ -67,4 +66,4 @@ for V2 in V2s:
         smu0.set_voltage(2, V1)
         VbAboveThresh[V2s.index(V2)].append(getState())
 
-sio.savemat("nFET-VbAboveThresh-Vout2.mat", mdict={'VbAboveThresh': VbAboveThresh})
+sio.savemat("pFET-VbAboveThresh-Vout2.mat", mdict={'VbAboveThresh': VbAboveThresh})
