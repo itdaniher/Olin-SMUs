89f5e3be77dbfe0db24742dbe8a415a2b35c1627
Ian Daniher <it.daniher@gmail.com>
2011-04-10 12:05:46 -0400

    added modified lab7

--- labs/lab7.py
+++ labs/lab7.py
@@ -13,57 +13,31 @@ smu1 = circuitsmu.smu(1)
 def getState():
 	return (smu0.get_voltage(1), smu0.get_current(1), smu0.get_voltage(2), smu0.get_current(2), smu1.get_voltage(1), smu1.get_current(1), smu1.get_voltage(2), smu1.get_current(2))
 
-smu1.set_voltage(1, 0)
-smu1.set_current(2, 0)
 #Vb = 4.30
 
-VbBelowThresh = [[], [], []]
+def getData(fileName, dataName):
+	smu1.set_voltage(1, 0)
+	smu1.set_current(2, 0)
+	dataSet = [[], [], []]
+	numV2s = 3
+	numV1s = 50
+	V2s = linspace(1, 4, 5).tolist()
+	for V2 in V2s:
+		smu0.set_voltage(1, V2)
+		for V1 in linspace(V2-.5, V2+.5, numV1s):
+			smu0.set_voltage(2, V1)
+			dataSet[V2s.index(V2)].append(getState())
+	sio.savemat(fileName, mdict={dataName: dataSet})
+	return dataSet
 
-numV2s = 3
-numV1s = 300
+plot([ a[0]-a[2] for a in dataSet[1] ], [ a[5] for a in dataSet[1] ] )
 
-V2s = linspace(1.5, 2.5, numV2s).tolist()
-V1s = linspace(2.0, 4, numV1s).tolist()
-
-for V2 in V2s:
-	smu0.set_voltage(1, V2)
-	for V1 in V1s:
-		smu0.set_voltage(2, V1)
-		VbBelowThresh[V2s.index(V2)].append(getState())
-
-sio.savemat("pFET-VbBelowThresh-Vout1.mat", mdict={'VbBelowThresh': VbBelowThresh})
-
+fileName = "pFET-VbBelowThresh-Vout1.mat"
+dataName = "VbBelowThresh"
+getData(fileName, dataName)
 #s11 - Vout2
 
-VbBelowThresh = [[], [], []]
-
-for V2 in V2s:
-	smu0.set_voltage(1, V2)
-	for V1 in V1s:
-		smu0.set_voltage(2, V1)
-		VbBelowThresh[V2s.index(V2)].append(getState())
-
-sio.savemat("pFET-VbBelowThresh-Vout2.mat", mdict={'VbBelowThresh': VbBelowThresh})
-
-#Vb = 4.125
+fileName = "pFET-VbBelowThresh-Vout2.mat"
+dataName = "VbBelowThresh"
+getData(fileName, dataName)
 #s11 - Vout1
-VbAboveThresh = [[], [], []]
-
-for V2 in V2s:
-    smu0.set_voltage(1, V2)
-    for V1 in V1s:
-        smu0.set_voltage(2, V1)
-        VbAboveThresh[V2s.index(V2)].append(getState())
-
-sio.savemat("pFET-VbAboveThresh-Vout1.mat", mdict={'VbAboveThresh': VbAboveThresh})
-
-#s11 - Vout2
-VbAboveThresh = [[], [], []]
-
-for V2 in V2s:
-    smu0.set_voltage(1, V2)
-    for V1 in V1s:
-        smu0.set_voltage(2, V1)
-        VbAboveThresh[V2s.index(V2)].append(getState())
-
-sio.savemat("pFET-VbAboveThresh-Vout2.mat", mdict={'VbAboveThresh': VbAboveThresh})
